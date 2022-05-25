8c8e8e921847cff3479fef4b640bbbd532d8a396
trumanwong <1415829005@qq.com>
2022-05-25 18:12:45 +0800

    modify cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => explode(',', env('ALLOWED_ORIGINS', '*')),
 
     'allowed_origins_patterns' => [],
 
